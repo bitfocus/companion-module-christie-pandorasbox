b788615f4d0dc90ab062e0b4c1e13ba5a1f0549c
kaminskihub <github@dk-production.de>
2019-07-13 14:46:25 +0200

    Add files via upload

--- christie-pandorasbox.js
+++ christie-pandorasbox.js
@@ -2,6 +2,9 @@ var tcp = require('../../tcp');
 var instance_skel = require('../../instance_skel');
 var debug;
 var log;
+var SeqStatus;
+var CurrentSeqID;
+var CurrentRemainingSeqID;
 
 function instance(system, id, config) {
 	let self = this;
@@ -20,6 +23,15 @@ function instance(system, id, config) {
 	self.CMD_SET_SITE_IP = 71;
 	self.CMD_APPLY_VIEW = 103;
 	self.CMD_SET_SEQ_SELECTION = 299;
+	self.CMD_GET_SEQ_TRANSPORTMODE = 72;
+	self.CMD_GET_SEQ_TIME = 73;	
+	self.CMD_GET_REMAINING_TIME_UNTIL_NEXT_CUE = 78;		
+	
+	self.feedbackstate = {
+			seqstate: 'Stop',
+			remainingQtime: 'Normal',
+		};	
+	
 
 	// super-constructor
 	instance_skel.apply(this, arguments);
@@ -60,11 +72,262 @@ instance.prototype.init = function() {
 	log = self.log;
 
 	self.init_tcp();
+	self.init_variables();
+	self.init_feedbacks();
 }
+
+
+instance.prototype.init_feedbacks = function() {
+	var self = this;
+
+	var feedbacks = {
+		state_color: {
+			label: 'Change color from Sequence State',
+			description: 'Change the colors of a bank according to the Seq state',
+			options: [
+				{
+					type: 'colorpicker',
+					label: 'Running: Foreground color',
+					id: 'run_fg',
+					default: self.rgb(255,255,255)
+				},
+				{
+					type: 'colorpicker',
+					label: 'Running: Background color',
+					id: 'run_bg',
+					default: self.rgb(0,100,0)
+				},
+				{
+					type: 'colorpicker',
+					label: 'Paused: Foreground color',
+					id: 'pause_fg',
+					default: self.rgb(255,255,255)
+				},
+				{
+					type: 'colorpicker',
+					label: 'Paused: Background color',
+					id: 'pause_bg',
+					default: self.rgb(170,80,0)
+				},
+				{
+					type: 'colorpicker',
+					label: 'Stopped: Foreground color',
+					id: 'stop_fg',
+					default: self.rgb(255,0,0)
+				},
+				{
+					type: 'colorpicker',
+					label: 'Stopped: Background color',
+					id: 'stop_bg',
+					default: self.rgb(0,0,0)
+				}
+			]
+		},
+		next_Q_color: {
+			label: 'Change color depending on Cue remaining Time',
+			description: 'Change the colors of a bank according to the Time until next Cue',
+			options: [
+				{
+					type: 'colorpicker',
+					label: 'Running more than 10 sec : Foreground color',
+					id: 'countdown_fg',
+					default: self.rgb(0,0,0)
+				},
+				{
+					type: 'colorpicker',
+					label: 'Running more than 10 sec: Background color',
+					id: 'countdown_bg',
+					default: self.rgb(255,255,0)
+				},
+				{
+					type: 'colorpicker',
+					label: 'Running 10 sec or less: Foreground color',
+					id: 'less10_fg',
+					default: self.rgb(255,0,0)
+				},
+				{
+					type: 'colorpicker',
+					label: 'Running 10 sec or less: Background color',
+					id: 'less10_bg',
+					default: self.rgb(255,153,51)
+				},
+				{
+					type: 'colorpicker',
+					label: 'Running 5 sec or less: Foreground color',
+					id: 'less05_fg',
+					default: self.rgb(255,255,255)
+				},
+				{
+					type: 'colorpicker',
+					label: 'Running 5 sec or less: Background color',
+					id: 'less05_bg',
+					default: self.rgb(255,0,0)
+				}
+			]
+		}
+	};
+
+	self.setFeedbackDefinitions(feedbacks);
+};
+
+instance.prototype.init_variables = function() {
+	var self = this;
+
+	var variables = [
+		{
+			label: 'Selected Sequence for Sequence Time',
+			name: 'seqid'
+		},
+		{
+			label: 'State of the timeline (Running, Stopped, Paused)',
+			name: 'seqstate'
+		},
+		{
+			label: 'Current time of Sequence (hh:mm:ss)',
+			name: 'seqtime'
+		},
+		{
+			label: 'Current time of Sequence (hours)',
+			name: 'seqtime_h'
+		},
+		{
+			label: 'Current time of Sequence (minutes)',
+			name: 'seqtime_m'
+		},
+		{
+			label: 'Current time of Sequence (seconds)',
+			name: 'seqtime_s'
+		},
+		{
+			label: 'Current time of Sequence (frames)',
+			name: 'seqtime_f'
+		},
+		{
+			label: 'Selected Sequence Remaining Cue Time',
+			name: 'nextqid'
+		},
+		{
+			label: 'Time until next Cue (hh:mm:ss)',
+			name: 'nextqtime'
+		},
+		{
+			label: 'Time until next Cue (hours)',
+			name: 'nextqtime_h'
+		},
+		{
+			label: 'Time until next Cue (minutes)',
+			name: 'nextqtime_m'
+		},
+		{
+			label: 'Time until next Cue (seconds)',
+			name: 'nextqtime_s'
+		},
+		{
+			label: 'Time until next Cue (frames)',
+			name: 'nextqtime_f'
+		},
+	];
+
+	self.updateNextQID(1);
+	self.updateSeqID(1);
+	self.setVariableDefinitions(variables);
+};
+
+instance.prototype.updateSeqID = function(changeseqid) {
+	var self = this;
+	self.setVariable('seqid', changeseqid);
+	CurrentSeqID = changeseqid;
+	self.send_getTimer(CurrentSeqID, CurrentRemainingSeqID);
+};
+
+instance.prototype.updateNextQID = function(changenextqid) {
+	var self = this;
+	self.setVariable('nextqid', changenextqid);
+	CurrentRemainingSeqID = changenextqid;
+	self.send_getTimer(CurrentSeqID, CurrentRemainingSeqID);
+};
+
+instance.prototype.incomingData = function(data) {
+	var self = this;
+
+	let magic = 'PBAU';
+	let domain = parseInt(self.config.domain);	
+	var receivebuffer = data;
+	
+	var rcv_cmd_id = 0;
+	var seq_state = 0;
+	var seq_h = '00';
+	var seq_m = '00';
+	var seq_s = '00';
+	var seq_f = '00';
+	var seq_time = '00:00:00:00'
+	var nextq_h = '00';
+	var nextq_m = '00';
+	var nextq_s = '00';
+	var nextq_f = '00';
+	var nextq_time = '00:00:00:00'
+
+	if (receivebuffer.toString('utf8',0,4) == magic && receivebuffer.readInt32BE(5) == domain) {
+		rcv_cmd_id = receivebuffer.readInt16BE(17);
+		switch (rcv_cmd_id) {
+		case 72 :
+			seq_state = receivebuffer.readInt32BE(19);
+			if (seq_state == 1){	
+				self.feedbackstate.state = 'Play';
+				self.setVariable('seqstate', 'Play');
+			} else if (seq_state == 2) {	
+				self.feedbackstate.state = 'Stop';
+				self.setVariable('seqstate', 'Stop');
+			} else if (seq_state == 3) {	
+				self.feedbackstate.state = 'Pause';
+				self.setVariable('seqstate', 'Pause');
+			};
+			self.checkFeedbacks('state_color');
+			break;
+					
+		case 73 :
+			seq_h = receivebuffer.readInt32BE(19);
+			seq_m = receivebuffer.readInt32BE(23);
+			seq_s = receivebuffer.readInt32BE(27);
+			seq_f = receivebuffer.readInt32BE(31);
+		
+			self.setVariable('seqtime_h', self.padZero(2,seq_h));
+			self.setVariable('seqtime_m', self.padZero(2,seq_m));
+			self.setVariable('seqtime_s', self.padZero(2,seq_s));
+			self.setVariable('seqtime_f', self.padZero(2,seq_f));
+			self.setVariable('seqtime', self.padZero(2,seq_h) +':'+self.padZero(2,seq_m)+':'+self.padZero(2,seq_s)+':'+self.padZero(2,seq_f));
+			break;
+
+		case 78 :
+			nextq_h = receivebuffer.readInt32BE(19);
+			nextq_m = receivebuffer.readInt32BE(23);
+			nextq_s = receivebuffer.readInt32BE(27);
+			nextq_f = receivebuffer.readInt32BE(31);
+		
+			self.setVariable('nextqtime_h', self.padZero(2,nextq_h));
+			self.setVariable('nextqtime_m', self.padZero(2,nextq_m));
+			self.setVariable('nextqtime_s', self.padZero(2,nextq_s));
+			self.setVariable('nextqtime_f', self.padZero(2,nextq_f));
+			self.setVariable('nextqtime', self.padZero(2,nextq_h) +':'+self.padZero(2,nextq_m)+':'+self.padZero(2,nextq_s)+':'+self.padZero(2,nextq_f));
+			
+			if (nextq_h == 0 && nextq_m == 0 && nextq_s < 5) {
+				self.feedbackstate.remainingQtime = 'Less05';
+			} else if  (nextq_h == 0 && nextq_m == 0 && nextq_s < 10) {
+				self.feedbackstate.remainingQtime = 'Less10';
+			} else {
+				self.feedbackstate.remainingQtime = 'Normal';
+			};
+			self.checkFeedbacks('next_Q_color');
+			break;
+		}
+	}	
+	//self.log("Buffer : " + receivebuffer + " - " + rcv_cmd_id);
+	//debug("Buffer : ", receivebuffer);
+};
 
 instance.prototype.init_tcp = function() {
 	var self = this;
-
+	
 	if (self.socket !== undefined) {
 		self.socket.destroy();
 		delete self.socket;
@@ -85,10 +348,14 @@ instance.prototype.init_tcp = function() {
 
 		self.socket.on('connect', function() {
 			self.status(self.STATE_OK);
+			self.init_variables();
+			//self.init_feedbacks();
 			debug("Connected");
 		})
 
-		self.socket.on('data', function(data) {});
+		self.socket.on('data', function(data) {
+			self.incomingData(data);
+		});
 	}
 }
 
@@ -107,6 +374,41 @@ instance.prototype.send = function(cmd) {
 		debug('Socket not connected');
 	}
 
+	return false;
+}
+
+instance.prototype.send_getTimer = function(gseqid, gnextqseqid) {
+	let self = this;
+	var gettimer;
+	var nextqtimeid = gnextqseqid;
+	var seqtimeid = gseqid;
+	
+	// Create all PBAutomation Commands
+	message1 = self.shortToBytes(self.CMD_GET_SEQ_TIME)
+				.concat(self.intToBytes(parseInt(seqtimeid)));
+			buf1 = Buffer.from(self.prependHeader(message1));
+	message2 = self.shortToBytes(self.CMD_GET_REMAINING_TIME_UNTIL_NEXT_CUE)
+				.concat(self.intToBytes(parseInt(nextqtimeid)));
+			buf2 = Buffer.from(self.prependHeader(message2));
+	message3 = self.shortToBytes(self.CMD_GET_SEQ_TRANSPORTMODE)
+				.concat(self.intToBytes(parseInt(seqtimeid)));
+			buf3 = Buffer.from(self.prependHeader(message3));
+			
+	if (self.isConnected()) {
+		clearInterval(self.gettimer_interval);
+		var gettime_period = 80; // ms
+		self.gettimer_interval = setInterval(gettimer, gettime_period);
+		function gettimer(){
+			self.socket.send(buf1);
+			setTimeout(function() {
+				self.socket.send(buf2);
+				setTimeout(function() {
+					self.socket.send(buf3)},18)
+				},30);
+		}
+	} else {
+		debug('Socket not connected');
+	}
 	return false;
 }
 
@@ -146,7 +448,7 @@ instance.prototype.config_fields = function() {
 		{
 			type: 'textinput',
 			id: 'domain',
-			width: 6,
+			width: 5,
 			label: 'Domain',
 			value: "0",
 			regex: self.REGEX_NUMBER
@@ -159,13 +461,16 @@ instance.prototype.config_fields = function() {
  */
 instance.prototype.destroy = function() {
 	let self = this;
+	
+	clearInterval(self.gettimer_interval);
+	setTimeout(function() {
+		debug('destroy', self.id);
 
-	debug('destroy', self.id);
-
-	if (self.socket !== undefined) {
-		self.socket.destroy();
-		delete self.socket;
-	}
+		if (self.socket !== undefined) {
+			self.socket.destroy();
+			delete self.socket;
+		}
+	},100);
 }
 
 /**
@@ -194,7 +499,7 @@ instance.prototype.actions = function(system) {
 					label: 'Sequence ID',
 					id: 'seq',
 					default: '1',
-					regex: self.REGEX_NUMBER
+					regex: '/^0*[1-9][0-9]*$/'
 				}
 			]
 		},
@@ -206,14 +511,14 @@ instance.prototype.actions = function(system) {
 					label: 'Sequence ID',
 					id: 'seq',
 					default: '1',
-					regex: self.REGEX_NUMBER
+					regex: '/^0*[1-9][0-9]*$/'
 				},
 				{
 					type: 'textinput',
 					label: 'Cue ID',
 					id: 'cueid',
 					default: '1',
-					regex: self.REGEX_NUMBER
+					regex: '/^0*[1-9][0-9]*$/'
 				}
 			]
 		},
@@ -225,7 +530,7 @@ instance.prototype.actions = function(system) {
 					label: 'Sequence ID',
 					id: 'seq',
 					default: '1',
-					regex: self.REGEX_NUMBER
+					regex: '/^0*[1-9][0-9]*$/'
 				},
 				{
 					type: 'dropdown',
@@ -247,7 +552,7 @@ instance.prototype.actions = function(system) {
 					label: 'Sequence ID',
 					id: 'seq',
 					default: '1',
-					regex: self.REGEX_NUMBER
+					regex: '/^0*[1-9][0-9]*$/'
 				},
 				{
 					type: 'dropdown',
@@ -269,7 +574,7 @@ instance.prototype.actions = function(system) {
 					label: 'Sequence ID',
 					id: 'seq',
 					default: '1',
-					regex: self.REGEX_NUMBER
+					regex: '/^0*[1-9][0-9]*$/'
 				}
 			]
 		},
@@ -281,7 +586,7 @@ instance.prototype.actions = function(system) {
 					label: 'View ID',
 					id: 'view',
 					default: '1',
-					regex: self.REGEX_NUMBER
+					regex: '/^0*[1-9][0-9]*$/'
 				}
 			]
 		},
@@ -294,7 +599,7 @@ instance.prototype.actions = function(system) {
 					label: 'SiteID',
 					id: 'site',
 					default: '1',
-					regex: self.REGEX_NUMBER
+					regex: '/^0*[1-9][0-9]*$/'
 				}
 			]
 		},
@@ -306,7 +611,7 @@ instance.prototype.actions = function(system) {
 					label: 'SiteID',
 					id: 'site',
 					default: '1',
-					regex: self.REGEX_NUMBER
+					regex: '/^0*[1-9][0-9]*$/'
 				},
 				{
 					type: 'textinput',
@@ -326,12 +631,48 @@ instance.prototype.actions = function(system) {
 					label: 'Sequence ID',
 					id: 'seq',
 					default: '1',
-					regex: self.REGEX_NUMBER
+					regex: '/^0*[1-9][0-9]*$/'
 				}
 			]
 		},
 		'clear_selection': { label: 'Clear Device Selection' },
-		'reset_all': { label: 'Reset All Values to Defaults' }
+		'reset_all': { label: 'Reset All Values to Defaults' },
+		'get_seq_state':   {
+			label: 'Get State of Sequence',
+			options: [
+				{
+					type: 'textinput',
+					label: 'Sequence ID',
+					id: 'seq',
+					default: '1',
+					regex: '/^0*[1-9][0-9]*$/'
+				}
+			]
+		},
+		'timer_seq_id': {
+			label: 'Change Timer Seq ID',
+			options: [
+				{
+					type: 'textinput',
+					label: 'Timer Sequence ID',
+					id: 'timerseq',
+					default: '1',
+					regex: '/^0*[1-9][0-9]*$/'
+				}
+			]
+		},
+		'nextq_seq_id': {
+			label: 'Change Remaining Cue Time Seq ID',
+			options: [
+				{
+					type: 'textinput',
+					label: 'Remaining Cue Time Sequence ID',
+					id: 'nextqseq',
+					default: '1',
+					regex: '/^0*[1-9][0-9]*$/'
+				}
+			]
+		}
 	});
 }
 
@@ -426,13 +767,29 @@ instance.prototype.action = function(action) {
 		case 'reset_all':
 			message = self.shortToBytes(self.CMD_RESET_ALL);
 			buf = Buffer.from(self.prependHeader(message));
+			break;		
+		
+		case 'timer_seq_id':
+			self.updateSeqID(opt.timerseq);
 			break;
+		
+		case 'nextq_seq_id':
+			self.updateNextQID(opt.nextqseq);
+			break;	
+			
 		}
 
-	if (buf !== undefined) {
-		self.send(buf);
+		if (buf !== undefined) {
+			self.send(buf);
 	}
+};
+
+instance.prototype.padZero = function(size, num) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
 }
+
 
 instance.prototype.intToBytes = function(int) {
 	return [
@@ -494,6 +851,52 @@ instance.prototype.prependHeader = function(body) {
 
 	return magic.concat(header).concat([checksum]).concat(body);
 }
+
+instance.prototype.feedback = function(feedback, bank) {
+	var self = this;
+
+	if (feedback.type == 'state_color') {
+		if (self.feedbackstate.seqstate == 'Play') {
+			return {
+				color: feedback.options.run_fg,
+				bgcolor: feedback.options.run_bg
+			};
+		}
+		else if (self.feedbackstate.seqstate == 'Pause') {
+			return {
+				color: feedback.options.pause_fg,
+				bgcolor: feedback.options.pause_bg
+			}
+		}
+		else if (self.feedbackstate.seqstate == 'Stop') {
+			return {
+				color: feedback.options.stop_fg,
+				bgcolor: feedback.options.stop_bg
+			}
+		}
+	};
+	if (feedback.type == 'next_Q_color') {
+		if (self.feedbackstate.remainingQtime == 'Normal') {
+			return {
+				color: feedback.options.countdown_fg,
+				bgcolor: feedback.options.countdown_bg
+			};
+		}
+		else if (self.feedbackstate.remainingQtime == 'Less10') {
+			return {
+				color: feedback.options.less10_fg,
+				bgcolor: feedback.options.less10_bg
+			}
+		}
+		else if (self.feedbackstate.remainingQtime == 'Less05') {
+			return {
+				color: feedback.options.less05_fg,
+				bgcolor: feedback.options.less05_bg
+			}
+		}
+	}
+	
+};
 
 instance_skel.extendedBy(instance);
 exports = module.exports = instance;
