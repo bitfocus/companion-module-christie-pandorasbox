3e2064ec0680520207c321c3209142306359aced
VideoBSO <video@staatsoper.de>
2023-07-20 23:10:47 +0200

    smaller things this time

--- src/config.js
+++ src/config.js
@@ -9,10 +9,9 @@ module.exports = {
  * @returns      The config fields for the module {Object}
  */
 	getConfigFields() {
-		let self = this;
 		return [
 			{
-				type: 'text',
+				type: 'static-text',
 				id: 'info',
 				width: 12,
 				label: 'Information',
@@ -21,13 +20,14 @@ module.exports = {
 			{
 				type: 'textinput',
 				id: 'host',
-				label: 'Target IP',
 				width: 6,
+				label: 'Target IP',
 				regex: Regex.IP
 			},
 			{
 				type: 'static-text',
 				id: 'port',
+				width: 5,
 				label: 'Port (static): ',
 				value: '6211'
 			},
